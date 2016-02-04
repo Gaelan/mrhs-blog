1e8c3f3e5724a52efc029c5d97adc5acd6d9884e
Douglas Urner <dlu@canishe.com>
2016-02-03 16:48:37 -0800

    Sort /users alphabetically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  after_action :verify_authorized
+  after_action  :verify_authorized
 
   def index
     authorize User
-    @users = policy_scope User.all
+    @users = policy_scope User.all.order(name: :asc)
   end
 end
