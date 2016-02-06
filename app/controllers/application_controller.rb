083469fb916a4f0bb5ea2cc6bed1cc8c4ad2420e
Gaelan Steele <gbs@canishe.com>
2016-02-06 15:03:05 -0800

    Set up robocop, run autocorrect.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,14 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  def new_session_path(scope)
+  def new_session_path(_scope)
     new_user_session_path
   end
 
   private
 
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = 'You are not authorized to perform this action.'
     redirect_to(request.referrer || root_path)
   end
 
