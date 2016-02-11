1a2bd7576630ec2b081872f7f715960ab8d3ba93
Douglas Urner <dlu@canishe.com>
2016-02-10 21:47:56 -0800

    Roughly working tasks index.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
     resources :posts
   end
 
+  resources :posts, only: [:index]
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_scope :user do
