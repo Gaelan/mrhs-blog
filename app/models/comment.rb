8e109dfcdc4f79156a369d0fae3a29ac8ff49ac9
Gaelan Steele <gbs@canishe.com>
2016-02-16 22:35:47 -0800

    Add support for private comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,8 @@
 class Comment < ActiveRecord::Base
-  after_create :debug
   belongs_to :commentable, polymorphic: true
   belongs_to :user
 
   has_many :comments, as: :commentable
 
   validates :user, presence: true
-
-  def debug
-    1+1
-  end
 end
