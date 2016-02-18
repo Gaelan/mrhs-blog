4e30f0bc06ead96d9ba242b6695b4f66b3ac540b
Douglas Urner <dlu@canishe.com>
2016-02-17 22:51:50 -0800

    Associate Stands with Tasks.

--- app/models/strand.rb
+++ app/models/strand.rb
@@ -1,3 +1,5 @@
 class Strand < ActiveRecord::Base
   belongs_to :objective
+  has_many :task_strands
+  has_many :tasks, through: :task_strands
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
   has_many :assessment_tasks
   has_many :assessments, through: :assessment_tasks
+  has_many :task_strands
+  has_many :strands, through: :task_strands
 end
