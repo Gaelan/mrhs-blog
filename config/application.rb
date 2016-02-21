371c4b90209aca795d233be1f0b29f58b5c488d3
Gaelan Steele <gbs@canishe.com>
2016-02-21 13:16:52 -0800

    Fix timestamps.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module MrhsBlog
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = '-08:00'
+    config.time_zone = "Pacific Time (US & Canada)" # XXX Nicer string?
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
