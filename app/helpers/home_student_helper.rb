c749db4c88f0bda66f476a73b8694c1b474a8f53
Douglas Urner <dlu@canishe.com>
2016-02-22 23:13:22 -0800

    Fix annunicator bug (count -> any?). Change home page.

--- app/helpers/home_student_helper.rb
+++ app/helpers/home_student_helper.rb
@@ -3,10 +3,11 @@ module HomeStudentHelper
   # Return a color string that can be used to set a CSS class to provide
   # visual feedback on progress.
   def annunciator(a)
-    if Post.where(user_id: @user.id, assessment_id: a.id, published: true).count
+    if Post.where(user_id: @user.id, assessment_id: a.id, published: true).any?
       # There is at least one published post against this assessment.
+      # binding.pry
       'green'
-    elsif Post.where(user_id: @user.id, assessment_id: a.id).count
+    elsif Post.where(user_id: @user.id, assessment_id: a.id).any?
       # There is at last one unpublished post aginst this assessment.
       if a.due_date > Time.now
         'orange'
