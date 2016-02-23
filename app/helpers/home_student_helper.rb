90b71c3e316095b6b1696db83b078274202fce54
Douglas Urner <dlu@canishe.com>
2016-02-23 06:30:17 -0800

    Comments.

--- app/helpers/home_student_helper.rb
+++ app/helpers/home_student_helper.rb
@@ -1,11 +1,13 @@
 #
 module HomeStudentHelper
   # Return a color string that can be used to set a CSS class to provide
-  # visual feedback on progress.
+  # visual feedback on progress. Argument is an Assessment.
+  #
+  # TODO: other sanity checks, nil bodies, no photos, small photos.
+  # TODO: be explicit about what you find in sanity checks. Hover?
   def annunciator(a)
     if Post.where(user_id: @user.id, assessment_id: a.id, published: true).any?
       # There is at least one published post against this assessment.
-      # binding.pry
       'green'
     elsif Post.where(user_id: @user.id, assessment_id: a.id).any?
       # There is at last one unpublished post aginst this assessment.
