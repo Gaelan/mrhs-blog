6040e19962e4adb84a48f5f92ecda0f5aaa95490
Douglas Urner <dlu@canishe.com>
2016-02-23 17:02:39 -0800

    Working on generating input for Illuminate.

--- app/helpers/assessments_helper.rb
+++ app/helpers/assessments_helper.rb
@@ -1,2 +1,20 @@
+#
 module AssessmentsHelper
+  # Generate the MRHS standard assessment name for each standard/objective
+  # assessed. The form is:
+  #
+  #   OBJ.Unit.Assessment#.ShortName -> B1.1.1.UP
+  #
+  # The short name is the first letter of each word in the Task name.
+  def illuminate_assessment_name(assessment)
+    assessment.strands.map do |strand|
+      sn = strand.objective.group + strand.number.to_s
+      unit = ''
+      f = assessment.formative? ? 'F' : ''
+      num = Assessment.where(section_id: assessment.section_id).count
+      short = assessment.tasks[0].title.gsub(/[ a-z]*/, '')
+
+      "#{sn}.#{unit}.#{f}#{num}.#{short}"
+    end.join '\n'
+  end
 end
