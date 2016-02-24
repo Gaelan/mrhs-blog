67afee200e27a33c81e53a942a8ed8eac4a221f6
Douglas Urner <dlu@canishe.com>
2016-02-23 21:24:13 -0800

    Functions for generating Illuminate assignments.

--- app/helpers/assessments_helper.rb
+++ app/helpers/assessments_helper.rb
@@ -1,20 +1,49 @@
 #
 module AssessmentsHelper
-  # Generate the MRHS standard assessment name for each standard/objective
+  # Returns a MRHS standard assessment name for each standard/objective
   # assessed. The form is:
   #
   #   OBJ.Unit.Assessment#.ShortName -> B1.1.1.UP
   #
-  # The short name is the first letter of each word in the Task name.
+  # The overall name is limited to 24 characters.
+  #
   def illuminate_assessment_name(assessment)
     assessment.strands.map do |strand|
+      limit = 24  # Max characters in assignment name.
       sn = strand.objective.group + strand.number.to_s
-      unit = ''
+      unit = 1    # TODO: use real unit numbers.
       f = assessment.formative? ? 'F' : ''
-      num = Assessment.where(section_id: assessment.section_id).count
-      short = assessment.tasks[0].title.gsub(/[ a-z]*/, '')
+      num = Assessment.where(
+        'section_id = ? AND due_date < ?',
+        assessment.section_id, assessment.due_date
+      ).count + 1
+      illuminate_name = "#{sn}.#{unit}.#{f}#{num}."
 
-      "#{sn}.#{unit}.#{f}#{num}.#{short}"
+      short = mogrify(assessment.tasks[0].title, limit - illuminate_name.length)
+
+      illuminate_name + short
     end.join '\n'
+  end
+
+  # Returns an assessment description suitable for pasting into Illuminate.
+  #
+  # Illuminate does not respect any formatting in the description, so we try
+  # to deliver only one paragraph of plain text along with the full assignment
+  # name.
+  #
+  def illuminate_assessment_description(assessment)
+    markdown assessment.tasks[0].body
+  end
+
+  # Shorten string:
+  # - Always elminiate white space, then
+  # - Truncate after a word, then
+  # - Acronymize (elminate lower case letters)
+  #
+  def mogrify(str, nchars)
+    if str.gsub!(/ */, '').length >= nchars
+      str.gsub!(/[a-z]*/, '')
+    end
+    str
   end
 end
