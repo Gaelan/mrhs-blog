5ce3305dd32d45c7c60fd6f4dc62b1d89df99937
Douglas Urner <dlu@canishe.com>
2016-02-28 22:30:35 -0800

    Fixed merge conflict.

--- app/models/strand.rb
+++ app/models/strand.rb
@@ -8,7 +8,20 @@ class Strand < ActiveRecord::Base
   has_many :assessments, through: :tasks
   has_many :rubrics, as: :rubricable
 
+<<<<<<< Updated upstream
   def to_s
     objective.group + number.to_s
+=======
+  def to_s(format: :long)
+    case :format
+    when :short
+      suffix = ''
+    when :long
+      suffix = ': ' + label
+    else
+      suffix = 'strand.to_s called with unknown format'
+    end
+    objective.group + number.to_s + suffix
+>>>>>>> Stashed changes
   end
 end
