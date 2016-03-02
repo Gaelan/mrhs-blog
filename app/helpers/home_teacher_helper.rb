0c7dc11fb4e5af999431ed10e46b049347aeea72
Douglas Urner <dlu@canishe.com>
2016-03-02 14:24:17 -0800

    Roughly working teacher dashboard.

--- app/helpers/home_teacher_helper.rb
+++ app/helpers/home_teacher_helper.rb
@@ -8,14 +8,37 @@ module HomeTeacherHelper
       # binding.pry
       {
         assessment: a,
-        status: posts.assessment_status(a)
+        status: assessment_status(a, posts)
       }
     end
-    binding.pry
-    status
+    # binding.pry
+    format_status(status)
   end
 
-  def assessment_status(id)
-    binding.pry
+  def assessment_status(aid, posts)
+    if posts.empty? then 'not-started'
+    else
+      post = posts.where(assessment: aid)
+      if post.length == 0
+        'not-started'
+      elsif post.length == 1
+        p = post[0]
+        if p.published
+          'submitted'
+        else
+          'started'
+        end
+      else
+        'not-started'
+      end
+    end
+  end
+
+  def format_status(as)
+    status = as.map do |s|
+      '<button class="status '"#{s[:status]}"'"> &nbsp; </button>'
+    end.join
+    #binding.pry
+    status.html_safe
   end
 end
