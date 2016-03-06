1177291ee5f45f86f1f3f897de1b4ec0edfc4308
Douglas Urner <dlu@canishe.com>
2016-03-05 21:29:03 -0800

    Fix e-mail address.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'knows its role in society' do
-    user = User.create! given_name: 'Dennis', family_name: 'Ritchie', email: 'dmr@research.att.com'
+    user = User.create! given_name: 'Dennis', family_name: 'Ritchie', email: 'dmr@bell-labs.com'
     user.roles.create role: :admin # Make the user an admin.
     expect(user).to be_admin # RSpec for "assert that user.admin? is true"
   end
 
-  it 'can be called by name' do
+  it 'knows its name' do
     user = User.new given_name: 'Alan', family_name: 'Turing'
     expect(user.name).to eq 'Alan Turing'
 
