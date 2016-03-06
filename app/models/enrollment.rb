eb490089ed0eccdf97a09658514c6b1bf345e1ed
Douglas Urner <dlu@canishe.com>
2016-03-05 22:36:56 -0800

    Adding validations and tests.

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,7 @@
 class Enrollment < ActiveRecord::Base
   belongs_to :section
   belongs_to :student, class_name: 'User'
+
+  validates :section, presence: true
+  validates :student, presence: true
 end
--- spec/models/rubric_spec.rb
+++ spec/models/rubric_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Rubric, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Rubric, type: :model do
+  describe 'validation' do
+    it 'fails without a level' do
+      rubric = Rubric.new(band: '7-8', criterion: 'Do good work.')
+      expect(rubric).not_to be_valid
+    end
+
+    it 'fails without a criterion' do
+      rubric = Rubric.new(band: '7-8', level: :unit)
+      expect(rubric).not_to be_valid
+    end
+
+    it 'fails without a band' do
+      rubric = Rubric.new(criterion: 'Do good work.', level: :unit)
+      expect(rubric).not_to be_valid
+    end
+
+    it 'succeeds with a band, criterion, and level' do
+      rubric = Rubric.new(band: '7-8', criterion: 'Do good work.', level: :unit)
+      expect(rubric).to be_valid
+    end
+  end
 end
