f1633cada73506be5dd9441696136ee32f79741a
Douglas Urner <dlu@canishe.com>
2016-03-29 18:13:15 -0700

    Add EXIFtool, and display photo metadata.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,22 @@
 module PostsHelper
+  # require 'mini_exiftool'
+
+  def image_exif(image)
+    exif = MiniExiftool.new image.file.path
+    # binding.pry
+    info = {
+      aperture: exif.aperturevalue,
+      datetimeoriginal: exif.datetimeoriginal,
+      flash: exif.flash,
+      iso: exif.iso,
+      make: exif.make,
+      model: exif.model,
+      shutterspeed: exif.shutterspeed,
+    }
+    @camera = "#{exif.make} #{exif.model}, #{exif.lensinfo}"
+    @exposure = "#{exif.shutterspeed.to_s} f/#{exif.aperturevalue} ISO: #{exif.iso}"
+    @capture = "#{exif.datetimeoriginal} #{exif.imagesize}"
+    binding.pry
+    exif
+  end
 end
